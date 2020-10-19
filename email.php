b0e54caebe8de4bfe8a7575d63283d88808a62ce
z1n-00 <69867926+z1n-00@users.noreply.github.com>
2020-10-19 13:58:43 +0630

    Update email.php

--- email.php
+++ email.php
@@ -2,14 +2,4 @@
 	echo"sending email";
 	echo"another email send";
 
-	echo"third line from online";
-	echo"third line from cli";
-
-	echo"fourth line form cli";
-	echo"fourth line from online";
-
-	echo"fifth line from online";
-
-	echo"fifith line from cli";
-
 
