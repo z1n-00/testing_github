32b5046e740fd729d231d43ea4a529f819d0b5fd
Zue In <zin008796@gmail.com>
2020-10-19 14:06:53 +0630

    updated email.php

--- email.php
+++ email.php
@@ -1,2 +1,15 @@
 <?php
-	echo "sending email";
+	echo"sending email";
+	echo"another email send";
+
+	echo"third line from online";
+	echo"third line from cli";
+
+	echo"fourth line form cli";
+	echo"fourth line from online";
+
+	echo"fifth line from online";
+
+	echo"fifith line from cli";
+
+
