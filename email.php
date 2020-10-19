2973dddfca41b5f7175a89d34770e4f68e94be86
Zue In <zin008796@gmail.com>
2020-10-19 14:00:46 +0630

    no message

--- email.php
+++ email.php
@@ -1,15 +1,2 @@
 <?php
-	echo"sending email";
-	echo"another email send";
-
-	echo"third line from online";
-	echo"third line from cli";
-
-	echo"fourth line form cli";
-	echo"fourth line from online";
-
-	echo"fifth line from online";
-
-	echo"fifith line from cli";
-
-
+	echo "sending email";
